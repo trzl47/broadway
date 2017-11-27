8c50863788091a1916a2b7906b0179a092d9c24d
trzl47 <brownty47@gmail.com>
2017-11-27 16:00:43 -0600

    complete docker dev env and update broken styling

--- services/web/src/app/Main.js
+++ services/web/src/app/Main.js
@@ -2,15 +2,15 @@
 import React, { Component } from 'react';
 // Components
 // styles
-import css from './broadway.css';
+import css from '../static/css/broadway.css';
 
-class Broadway extends Component {
+class Main extends Component {
 	render() {
 		return(
-			<div className={[css.broadway].join(' ')}>
-				<div className={[css.header].join(' ')}>
+			<div className={'header'}>
+				<div className={['header'].join(' ')}>
 					<div className={['container'].join(' ')}>
-						<ul className={[css.nav].join(' ')}>
+						<ul className={['nav'].join(' ')}>
 							<li><a href="/">About</a></li>
 							<li><a href="/">Work</a></li>
 							<li><a href="/">Team</a></li>
@@ -18,38 +18,38 @@ class Broadway extends Component {
 						</ul>
 					</div>
 				</div>
-				<div className={['jumbotron', css.jumbotron].join(' ')}>
+				<div className={['jumbotron', 'jumbotron'].join(' ')}>
 					<div className={['container'].join(' ')}>
-						<div className={[css.main].join(' ')}>
+						<div className={['main'].join(' ')}>
 							<h1>We are Broadway</h1>
 							<a href="/" className={['btn-main', css['btn-main']].join(' ')} rel="style" type="text/css">Get Started</a>
 						</div>
 					</div>
 				</div>
-				<div className={[css.supporting].join(' ')}>
+				<div className={['supporting'].join(' ')}>
 					<div className={['container'].join(' ')}>
-						<div className={[css.col].join(' ')}>
-							<img alt='' src={require('./assets/design.svg')} />
+						<div className={['col'].join(' ')}>
+							<img alt='' src={require('../static/img/design.svg')} />
 							<h2>Design</h2>
 							<p>Make your projects look great and interact beautifully.</p>
 							<a href="/" className={['btn-default', css['btn-default']].join(' ')} rel="style" type="text/css">Learn More</a>
 						</div>
-						<div className={[css.col].join(' ')}>
-							<img alt='' src={require('./assets/develop.svg')} />
+						<div className={['col'].join(' ')}>
+							<img alt='' src={require('../static/img/develop.svg')} />
 							<h2>Develop</h2>
 							<p>Use modern tools to turn your design into a web site</p>
 							<a href="/" className={['btn-default', css['btn-default']].join(' ')} rel="style" type="text/css">Learn More</a>
 						</div>
-						<div className={[css.col].join(' ')}>
-							<img alt='' src={require('./assets/deploy.svg')} />
+						<div className={['col'].join(' ')}>
+							<img alt='' src={require('../static/img/deploy.svg')} />
 							<h2>Deploy</h2>
 							<p>Use modern tools to turn your design into a web site</p>
 							<a href="/" className={['btn-default', css['btn-default']].join(' ')} rel="style" type="text/css">Learn More</a>
 						</div>
 					</div>
-					<div className={[css.clearfix].join(' ')}></div>
+					<div className={['clearfix'].join(' ')}></div>
 				</div>
-				<div className={[css.footer].join(' ')}>
+				<div className={['footer'].join(' ')}>
 					<div className={['container'].join(' ')}>
 						<p>&copy; Broadway 2015</p>
 					</div>
@@ -59,4 +59,4 @@ class Broadway extends Component {
 	}
 }
 
-export default Broadway;
+export default Main;
